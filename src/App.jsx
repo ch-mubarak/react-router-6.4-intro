3e567eb8d45e4858d8c33e7d51f010bf921bb31c
chmubarak <chmohamedmubarak@gmail.com>
2022-11-16 19:03:51 +0530

    learned router 6.4 basics

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,38 @@
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import {
+  RouterProvider,
+  createBrowserRouter,
+  createRoutesFromElements,
+  Route,
+} from "react-router-dom";
+import BlogLayout from "./pages/BlogLayout";
+import BlogPostsPage, { loader as blogPostLoader } from "./pages/BlogPosts";
+import NewPostPage,{action as newPostAction} from "./pages/NewPost";
+import PostDetailPage, { loader as postDetailLoader } from "./pages/PostDetail";
+import RootLayout from "./components/RootLayout";
+import WelcomePage from "./pages/Welcome";
+import ErrorPage from "./pages/ErrorPage";
 
-import BlogLayout from './pages/BlogLayout';
-import BlogPostsPage from './pages/BlogPosts';
-import NewPostPage from './pages/NewPost';
-import PostDetailPage from './pages/PostDetail';
-import RootLayout from './components/RootLayout';
-import WelcomePage from './pages/Welcome';
-
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route element={<RootLayout />} errorElement={<ErrorPage />}>
+      <Route index element={<WelcomePage />} />
+      <Route path="/blog" element={<BlogLayout />}>
+        <Route index element={<BlogPostsPage />} loader={blogPostLoader} />
+        <Route
+          path=":id"
+          element={<PostDetailPage />}
+          loader={postDetailLoader}
+        />
+      </Route>
+      <Route path="/blog/new" element={<NewPostPage />} action={newPostAction}/>
+    </Route>
+  )
+);
 function App() {
   return (
-    <BrowserRouter>
-      <RootLayout>
-        <Routes>
-          <Route path="/" element={<WelcomePage />} />
-          <Route path="/blog" element={<BlogLayout />}>
-            <Route index element={<BlogPostsPage />} />
-            <Route path=":id" element={<PostDetailPage />} />
-          </Route>
-          <Route path="/blog/new" element={<NewPostPage />} />
-        </Routes>
-      </RootLayout>
-    </BrowserRouter>
+    <>
+      <RouterProvider router={router} />
+    </>
   );
 }
 
